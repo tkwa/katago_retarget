4ad2292e299052d635b5e0848cddaf136bb19ad5
tkwa <kwathomas0@gmail.com>
2023-10-19 01:23:13 +0000

    change annotate_sgf to use analysis engine

--- annotate_sgf.py
+++ annotate_sgf.py
@@ -18,6 +18,20 @@ import cProfile
 import pstats
 sys.path.append("/home/ubuntu/katago_pessimize/KataGo/python")
 
+# from analysis engine code
+import argparse
+import json
+import subprocess
+import time
+from threading import Thread
+import sgfmill
+import sgfmill.boards
+import sgfmill.ascii_boards
+from typing import Tuple, List, Optional, Union, Literal
+import io
+
+Color = Union[Literal["b"],Literal["w"]]
+Move = Union[Literal["pass"],Tuple[int,int]]
 
 from sgfmill import sgf
 from KataGo.python.board import Board
@@ -56,223 +70,152 @@ class GameState:
         self.board.play(pla, loc)
         self.moves.append((pla, loc))
         self.boards.append(self.board.copy())
-        # print(f"Now {len(self.moves)} moves and {len(self.boards)} boards")
 
 
-# %%
+def sgfmill_to_str(move: Move) -> str:
+    if move == "pass":
+        return "pass"
+    (y,x) = move
+    return "ABCDEFGHJKLMNOPQRSTUVWXYZ"[x] + str(y+1)
 
-# copied from play
-def get_outputs(gss:GameState | List[GameState], rules):
-    if type(gss) is GameState:
-        gss = [gss]
-    with torch.no_grad():
-        model.eval()
+class KataGo:
 
-        batch_size = len(gss)
-        bin_input_data = np.zeros(shape=[batch_size]+model.bin_input_shape, dtype=np.float32)
-        global_input_data = np.zeros(shape=[1]+model.global_input_shape, dtype=np.float32)
-        pla = [gs.board.pla for gs in gss]
-        opp = [Board.get_opp(p) for p in pla]
-        move_idx = [len(gs.moves) for gs in gss]
-        # This function assumes N(HW)C order but we actually use NCHW order, so work with it and revert
-        bin_input_data = np.transpose(bin_input_data,axes=(0,2,3,1))
-        bin_input_data = bin_input_data.reshape([batch_size,1,pos_len*pos_len,-1])
-        featureses = [Features(model.config,pos_len) for i in range(batch_size)]
-        for i, gs in enumerate(gss):
-            featureses[i].fill_row_features(gs.board,pla[i],opp[i],gs.boards,gs.moves,move_idx[i],rules,bin_input_data[i],global_input_data,idx=0)
-        bin_input_data = bin_input_data.reshape([batch_size,pos_len,pos_len,-1])
-        bin_input_data = np.transpose(bin_input_data,axes=(0,3,1,2))
-
-        # Currently we don't actually do any symmetries
-        # symmetry = 0
-        # model_outputs = model(apply_symmetry(batch["binaryInputNCHW"],symmetry),batch["globalInputNC"])
-
-        global_input_data = repeat(global_input_data, '1 ... -> batch ...', batch=batch_size)
-        # print(bin_input_data.shape)
-        # print(global_input_data.shape)
-        model_outputs = model(
-            torch.tensor(bin_input_data, dtype=torch.float32).to(DEVICE),
-            torch.tensor(global_input_data, dtype=torch.float32).to(DEVICE),
+    def __init__(self, katago_path: str, config_path: str, model_path: str):
+        self.query_counter = 0
+        katago = subprocess.Popen(
+            [katago_path, "analysis", "-config", config_path, "-model", model_path],
+            stdin=subprocess.PIPE,
+            stdout=subprocess.PIPE,
+            stderr=subprocess.PIPE,
         )
-        outputs = model.postprocess_output(model_outputs)
-        (
-            policy_logits,      # N, num_policy_outputs, move
-            value_logits,       # N, {win,loss,noresult}
-            td_value_logits,    # N, {long, mid, short} {win,loss,noresult}
-            pred_td_score,      # N, {long, mid, short}
-            ownership_pretanh,  # N, 1, y, x
-            pred_scoring,       # N, 1, y, x
-            futurepos_pretanh,  # N, 2, y, x
-            seki_logits,        # N, 4, y, x
-            pred_scoremean,     # N
-            pred_scorestdev,    # N
-            pred_lead,          # N
-            pred_variance_time, # N
-            pred_shortterm_value_error, # N
-            pred_shortterm_score_error, # N
-            scorebelief_logits, # N, 2 * (self.pos_len*self.pos_len + EXTRA_SCORE_DISTR_RADIUS)
-        ) = (x[0] for x in outputs[0]) # N = 0
+        self.katago = katago
 
-        policy0 = torch.nn.functional.softmax(policy_logits[0,:],dim=0).cpu().numpy()
-        policy1 = torch.nn.functional.softmax(policy_logits[1,:],dim=0).cpu().numpy()
-        value = torch.nn.functional.softmax(value_logits,dim=0).cpu().numpy()
-        td_value = torch.nn.functional.softmax(td_value_logits[0,:],dim=0).cpu().numpy()
-        td_value2 = torch.nn.functional.softmax(td_value_logits[1,:],dim=0).cpu().numpy()
-        td_value3 = torch.nn.functional.softmax(td_value_logits[2,:],dim=0).cpu().numpy()
-        scoremean = pred_scoremean.cpu().item()
-        td_score = pred_td_score.cpu().numpy()
-        scorestdev = pred_scorestdev.cpu().item()
-        lead = pred_lead.cpu().item()
-        vtime = pred_variance_time.cpu().item()
-        estv = math.sqrt(pred_shortterm_value_error.cpu().item())
-        ests = math.sqrt(pred_shortterm_score_error.cpu().item())
-        ownership = torch.tanh(ownership_pretanh).cpu().numpy()
-        scoring = pred_scoring.cpu().numpy()
-        futurepos = torch.tanh(futurepos_pretanh).cpu().numpy()
-        seki_probs = torch.nn.functional.softmax(seki_logits[0:3,:,:],dim=0).cpu().numpy()
-        seki = seki_probs[1] - seki_probs[2]
-        seki2 = torch.sigmoid(seki_logits[3,:,:]).cpu().numpy()
-        scorebelief = torch.nn.functional.softmax(scorebelief_logits,dim=0).cpu().numpy()
+        def printforever():
+            while katago.poll() is None:
+                data = katago.stderr.readline()
+                # time.sleep(0)
+                if data:
+                    print("KataGo: ", data.decode(), end="")
+            data = katago.stderr.read()
+            if data:
+                print("KataGo: ", data.decode(), end="")
+        self.stderrthread = Thread(target=printforever)
+        self.stderrthread.start()
 
-    # for gs, features in zip(gss, featureses):
-    #     board = gs.board
-    #     moves_and_probs0 = []
-    #     for i in range(len(policy0)):
-    #         move = features.tensor_pos_to_loc(i,board)
-    #         if i == len(policy0)-1:
-    #             moves_and_probs0.append((Board.PASS_LOC,policy0[i]))
-    #         elif board.would_be_legal(board.pla,move):
-    #             moves_and_probs0.append((move,policy0[i]))
+    def close(self):
+        self.katago.stdin.close()
 
-    #     moves_and_probs1 = []
-    #     for i in range(len(policy1)):
-    #         move = features.tensor_pos_to_loc(i,board)
-    #         if i == len(policy1)-1:
-    #             moves_and_probs1.append((Board.PASS_LOC,policy1[i]))
-    #         elif board.would_be_legal(board.pla,move):
-    #             moves_and_probs1.append((move,policy1[i]))
 
-    #     ownership_flat = ownership.reshape([features.pos_len * features.pos_len])
-    #     ownership_by_loc = []
-    #     board = gss.board
-    #     for y in range(board.size):
-    #         for x in range(board.size):
-    #             loc = board.loc(x,y)
-    #             pos = features.loc_to_tensor_pos(loc,board)
-    #             if board.pla == Board.WHITE:
-    #                 ownership_by_loc.append((loc,ownership_flat[pos]))
-    #             else:
-    #                 ownership_by_loc.append((loc,-ownership_flat[pos]))
+    def query(self, initial_board: sgfmill.boards.Board, moves: List[Tuple[Color,Move]], komi: float, max_visits=None):
+        query = {}
 
-    #     scoring_flat = scoring.reshape([features.pos_len * features.pos_len])
-    #     scoring_by_loc = []
-    #     board = gss.board
-    #     for y in range(board.size):
-    #         for x in range(board.size):
-    #             loc = board.loc(x,y)
-    #             pos = features.loc_to_tensor_pos(loc,board)
-    #             if board.pla == Board.WHITE:
-    #                 scoring_by_loc.append((loc,scoring_flat[pos]))
-    #             else:
-    #                 scoring_by_loc.append((loc,-scoring_flat[pos]))
+        query["id"] = str(self.query_counter)
+        self.query_counter += 1
 
-    #     futurepos0_flat = futurepos[0,:,:].reshape([features.pos_len * features.pos_len])
-    #     futurepos0_by_loc = []
-    #     board = gss.board
-    #     for y in range(board.size):
-    #         for x in range(board.size):
-    #             loc = board.loc(x,y)
-    #             pos = features.loc_to_tensor_pos(loc,board)
-    #             if board.pla == Board.WHITE:
-    #                 futurepos0_by_loc.append((loc,futurepos0_flat[pos]))
-    #             else:
-    #                 futurepos0_by_loc.append((loc,-futurepos0_flat[pos]))
+        query["moves"] = [(color,sgfmill_to_str(move)) for color, move in moves]
+        query["initialStones"] = []
+        for y in range(initial_board.side):
+            for x in range(initial_board.side):
+                color = initial_board.get(y,x)
+                if color:
+                    query["initialStones"].append((color,sgfmill_to_str((y,x))))
+        query["rules"] = "Chinese"
+        query["komi"] = komi
+        query["boardXSize"] = initial_board.side
+        query["boardYSize"] = initial_board.side
+        query["includePolicy"] = False
+        if max_visits is not None:
+            query["maxVisits"] = max_visits
 
-    #     futurepos1_flat = futurepos[1,:,:].reshape([features.pos_len * features.pos_len])
-    #     futurepos1_by_loc = []
-    #     board = gss.board
-    #     for y in range(board.size):
-    #         for x in range(board.size):
-    #             loc = board.loc(x,y)
-    #             pos = features.loc_to_tensor_pos(loc,board)
-    #             if board.pla == Board.WHITE:
-    #                 futurepos1_by_loc.append((loc,futurepos1_flat[pos]))
-    #             else:
-    #                 futurepos1_by_loc.append((loc,-futurepos1_flat[pos]))
+        self.katago.stdin.write((json.dumps(query) + "\n").encode())
+        self.katago.stdin.flush()
 
-    #     seki_flat = seki.reshape([features.pos_len * features.pos_len])
-    #     seki_by_loc = []
-    #     board = gss.board
-    #     for y in range(board.size):
-    #         for x in range(board.size):
-    #             loc = board.loc(x,y)
-    #             pos = features.loc_to_tensor_pos(loc,board)
-    #             if board.pla == Board.WHITE:
-    #                 seki_by_loc.append((loc,seki_flat[pos]))
-    #             else:
-    #                 seki_by_loc.append((loc,-seki_flat[pos]))
+        # print(json.dumps(query))
 
-    #     seki_flat2 = seki2.reshape([features.pos_len * features.pos_len])
-    #     seki_by_loc2 = []
-    #     board = gss.board
-    #     for y in range(board.size):
-    #         for x in range(board.size):
-    #             loc = board.loc(x,y)
-    #             pos = features.loc_to_tensor_pos(loc,board)
-    #             seki_by_loc2.append((loc,seki_flat2[pos]))
+    def get_response(self):
+        line = ""
+        while line == "":
+            if self.katago.poll():
+                time.sleep(1)
+                raise Exception("Unexpected katago exit")
+            # time.sleep(0.002)
+            line = self.katago.stdout.readline()
+            # print(f"Successfully read line {line}")
+            line = line.decode().strip()
+            # print("Got: " + line)
+        response = json.loads(line)
+        return response
+    
+    def get_all_responses(self):
+        lines = ''
+        if self.katago.poll():
+            time.sleep(1)
+            raise Exception("Unexpected katago exit")
+        lines = self.katago.stdout.readlines()
+        if lines == '':
+            raise Exception("No line")
+        # print(f"Successfully read line {line}")
+        lines = [line.decode().strip() for line in lines]
+        response = [json.loads(line) for line in lines]
+        return response
 
-    #     moves_and_probs = sorted(moves_and_probs0, key=lambda moveandprob: moveandprob[1], reverse=True)
-    #     # Generate a random number biased small and then find the appropriate move to make
-    #     # Interpolate from moving uniformly to choosing from the triangular distribution
-    #     alpha = 1
-    #     beta = 1 + math.sqrt(max(0,len(gss.moves)-20))
-    #     r = np.random.beta(alpha,beta)
-    #     probsum = 0.0
-    #     i = 0
-    #     genmove_result = Board.PASS_LOC
-    #     while True:
-    #         (move,prob) = moves_and_probs[i]
-    #         probsum += prob
-    #         if i >= len(moves_and_probs)-1 or probsum > r:
-    #             genmove_result = move
-    #             break
-    #         i += 1
+if __name__ == "__main__":
+    # description = """
+    # Example script showing how to run KataGo analysis engine and query it from python.
+    # """
+    # parser = argparse.ArgumentParser(description=description)
+    # parser.add_argument(
+    #     "-katago-path",
+    #     help="Path to katago executable",
+    #     required=True,
+    # )
+    # parser.add_argument(
+    #     "-config-path",
+    #     help="Path to KataGo analysis config (e.g. cpp/configs/analysis_example.cfg in KataGo repo)",
+    #     required=True,
+    # )
+    # parser.add_argument(
+    #     "-model-path",
+    #     help="Path to neural network .bin.gz file",
+    #     required=True,
+    # )
+    # args = vars(parser.parse_args())
+    # print(args)
 
-        return {
-            "policy0": policy0,
-            "policy1": policy1,
-            # "moves_and_probs0": moves_and_probs0,
-            # "moves_and_probs1": moves_and_probs1,
-            "value": value,
-            "td_value": td_value,
-            "td_value2": td_value2,
-            "td_value3": td_value3,
-            "scoremean": scoremean,
-            "td_score": td_score,
-            "scorestdev": scorestdev,
-            "lead": lead,
-            "vtime": vtime,
-            "estv": estv,
-            "ests": ests,
-            "ownership": ownership,
-            # "ownership_by_loc": ownership_by_loc,
-            "scoring": scoring,
-            # "scoring_by_loc": scoring_by_loc,
-            "futurepos": futurepos,
-            # "futurepos0_by_loc": futurepos0_by_loc,
-            # "futurepos1_by_loc": futurepos1_by_loc,
-            "seki": seki,
-            # "seki_by_loc": seki_by_loc,
-            "seki2": seki2,
-            # "seki_by_loc2": seki_by_loc2,
-            "scorebelief": scorebelief,
-            # "genmove_result": genmove_result
-        }
+    # katago = KataGo(args["katago_path"], args["config_path"], args["model_path"])
+
+    board = sgfmill.boards.Board(19)
+    komi = 6.5
+    # moves = [("b",(3,3))]
+
+    # displayboard = board.copy()
+    # for color, move in moves:
+    #     if move != "pass":
+    #         row,col = move
+    #         displayboard.play(row,col,color)
+    # print(sgfmill.ascii_boards.render_board(displayboard))
+
+    # print("Query result: ")
+    # for i in range(10000):
+    #     q = katago.query(board, moves, komi)
+
+    # katago.close()
 
 # %%
 
-def get_training_data_from_sgf(sgf_filename):
+def loc_to_query_loc(board, loc):
+    x, y = board.loc_x(loc), board.loc_y(loc)
+    return sgfmill_to_str((y,x)) # am I switching coordinates here?
+# %%
+
+KATAGO_PATH = "/home/ubuntu/katago_pessimize/kg_release/katago"
+CONFIG_PATH = "/home/ubuntu/katago_pessimize/analysis.cfg"
+MODEL_PATH = "/home/ubuntu/katago_pessimize/kata1-b18c384nbt-s7709731328-d3715293823.bin.gz"
+
+elapsed_out = []
+def get_training_data_from_sgf(katago:KataGo, sgf_filename):
     global rules
+    global elapsed_out
     metadata, setup, moves, rules = load_sgf_moves_exn(sgf_filename)
     print(f"Loaded {sgf_filename}")
     print(f"Metadata: {metadata}")
@@ -280,36 +223,52 @@ def get_training_data_from_sgf(sgf_filename):
     print(f"Setup: {setup}")
     print(f"Moves: {moves}")
 
+    # katago = KataGo(args["katago_path"], args["config_path"], args["model_path"])
+    query_board = sgfmill.boards.Board(19)
+    komi = 6.5
+
     gs = GameState(metadata.size)
     # for move in setup:
     #     gs.play(move[0], move[1])
     # print(gs.board.to_string())
     values = []
     move_values = []
+    start_time = time.time()
+    queries = 0
     for game_move in tqdm(moves[:5]):
         # print(f"playing {game_move}")
         gs.play(game_move[0], game_move[1])
         board_str = '\n' + gs.board.to_string().strip()
         # print(board_str)
 
-        outputs = get_outputs(gs, rules)
-        values.append(outputs["value"][0] + outputs["value"][2]/2)
-
         # make all possible moves from this position
         this_values = dict()
-        gss = []
         for move in range(metadata.size ** 2):
+            move = move + 19 + 1
             if gs.board.would_be_legal(gs.board.pla, move):
-                gs_copy = deepcopy(gs)
-                gs_copy.play(gs.board.pla, move)
-                gss.append(gs_copy)
-        outputs = get_outputs(gss, rules)
-        this_values[move] = outputs["value"][0] + outputs["value"][2]/2
+                query_moves = [('_bw'[color], (gs.board.loc_x(loc), gs.board.loc_y(loc))) for color, loc in gs.moves + [(gs.board.pla, move)]]
+                # print(query_moves)
+                katago.query(query_board, query_moves, komi)
+                queries += 1
+                # if queries >= 100:
+                #     queries = 0
+    # outputs = katago.get_all_responses()
+    time.sleep(4)
+    for _ in range(queries):
+        before_out = time.time()
+        output = katago.get_response()
+        elapsed_out.append(time.time() - before_out)
+        if 'rootInfo' in output: values.append(output['rootInfo']["winrate"])
+    # for output in outputs:
+
+                # for move in range(metadata.size ** 2):
+
+            
         move_values.append(this_values)
 
-
-
-    print(values)
+    end_time = time.time()
+    print(f"Time: {end_time - start_time:.4f}")
+    # print(values)
     return values, move_values, gs
 
 
@@ -317,10 +276,24 @@ def get_training_data_from_sgf(sgf_filename):
 sgf_filename = os.path.join(SGF_DIR, os.listdir(SGF_DIR)[2])
 # sgf_filename = "blood_vomit.sgf"
 
-cProfile.run('values, move_values, gs = get_training_data_from_sgf(sgf_filename)', 'output.pstats')
+katago = KataGo(KATAGO_PATH, CONFIG_PATH, MODEL_PATH)
+try:
+    cProfile.run('values, move_values, gs = get_training_data_from_sgf(katago, sgf_filename)', 'output.pstats')
+    # values, move_values, gs = get_training_data_from_sgf(katago, sgf_filename)
+    katago.close()
+except KeyboardInterrupt:
+    katago.close()
+    raise KeyboardInterrupt
+except Exception as e:
+    print(e)
+    katago.close()
+    raise e
 
 # print(gs.board.to_string())
 
+# %%
+
+sns.lineplot(elapsed_out)
 # %%
 
 # Create a pstats object
