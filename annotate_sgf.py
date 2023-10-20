0670056aabde3e07dd5f8bc75aa19e724a17b0a2
tkwa <kwathomas0@gmail.com>
2023-10-20 01:11:00 +0000

    fix several issues with annotate_sgf

--- annotate_sgf.py
+++ annotate_sgf.py
@@ -81,10 +81,10 @@ def sgfmill_to_str(move: Move) -> str:
 
 class KataGo:
 
-    def __init__(self, katago_path: str, config_path: str, model_path: str, filename: str):
+    def __init__(self, katago_path: str, config_path: str, model_path: str, filename: str, verbose=False):
         self.query_counter = 0
         # open file to write stdout to
-        self.out_file = open(os.path.join(ANNOTATION_DIR, filename + '.stdout'), 'w')
+        self.out_file = open(filename, 'w')
         katago = subprocess.Popen(
             [katago_path, "analysis", "-config", config_path, "-model", model_path],
             stdin=subprocess.PIPE,
@@ -93,12 +93,13 @@ class KataGo:
             # bufsize=1,
         )
         self.katago = katago
+        self.verbose = verbose
 
         def printforever():
             while katago.poll() is None:
                 data = katago.stderr.readline()
                 # time.sleep(0)
-                if data:
+                if data and verbose:
                     print("KataGo: ", data.decode(), end="")
             data = katago.stderr.read()
             if data:
@@ -119,10 +120,15 @@ class KataGo:
                     line = data.decode().strip()
                     json_object = json.loads(line)
                     out_object = {}
-                    out_object['id'] = json_object['id']
+                    id = json_object['id'].split()
+                    out_object['id'] = id[0]
+                    out_object['pla'] = id[1]
+                    out_object['loc'] = id[2] + ' ' + id[3]
+                    out_object['score'] = json_object['rootInfo']['scoreLead']
+                    out_object['turnNumber'] = json_object['turnNumber']
                     out_object['winrate'] = json_object['rootInfo']['winrate']
                     self.out_file.write(json.dumps(out_object) + '\n')
-            print(f"Done writing {self.n_writes} lines")
+            print(f"Done writing {self.n_writes} lines to {filename}")
             if self.katago.stdout.closed: return
             data = katago.stdout.read()
             if data:
@@ -135,12 +141,12 @@ class KataGo:
         self.katago.stdin.close()
         self.stdoutthread.join()
         self.out_file.close()
-
+        return self.n_writes
 
     def query(self, initial_board: sgfmill.boards.Board, moves: List[Tuple[Color,Move]], komi: float, max_visits=None):
         query = {}
 
-        query["id"] = str(self.query_counter)
+        query["id"] = f"{self.query_counter} {moves[-1][0]} {moves[-1][1][0]} {moves[-1][1][1]}"
         self.query_counter += 1
 
         query["moves"] = [(color,sgfmill_to_str(move)) for color, move in moves]
@@ -163,78 +169,6 @@ class KataGo:
         self.katago.stdin.write(query)
         self.katago.stdin.flush()
 
-        # print(json.dumps(query))
-
-    def get_response(self):
-        line = ""
-        while line == "":
-            self.katago.stdout.flush()
-            if self.katago.poll():
-                time.sleep(1)
-                raise Exception("Unexpected katago exit")
-            # time.sleep(0.002)
-            line = self.katago.stdout.readline()
-            # print(f"Successfully read line {line}")
-            line = line.decode().strip()
-            # print("Got: " + line)
-        response = json.loads(line)
-        return response
-    
-    def get_all_responses(self):
-        lines = ''
-        if self.katago.poll():
-            time.sleep(1)
-            raise Exception("Unexpected katago exit")
-        lines = self.katago.stdout.readlines()
-        if lines == '':
-            raise Exception("No line")
-        # print(f"Successfully read line {line}")
-        lines = [line.decode().strip() for line in lines]
-        response = [json.loads(line) for line in lines]
-        return response
-
-if __name__ == "__main__":
-    # description = """
-    # Example script showing how to run KataGo analysis engine and query it from python.
-    # """
-    # parser = argparse.ArgumentParser(description=description)
-    # parser.add_argument(
-    #     "-katago-path",
-    #     help="Path to katago executable",
-    #     required=True,
-    # )
-    # parser.add_argument(
-    #     "-config-path",
-    #     help="Path to KataGo analysis config (e.g. cpp/configs/analysis_example.cfg in KataGo repo)",
-    #     required=True,
-    # )
-    # parser.add_argument(
-    #     "-model-path",
-    #     help="Path to neural network .bin.gz file",
-    #     required=True,
-    # )
-    # args = vars(parser.parse_args())
-    # print(args)
-
-    # katago = KataGo(args["katago_path"], args["config_path"], args["model_path"])
-
-    board = sgfmill.boards.Board(19)
-    komi = 6.5
-    # moves = [("b",(3,3))]
-
-    # displayboard = board.copy()
-    # for color, move in moves:
-    #     if move != "pass":
-    #         row,col = move
-    #         displayboard.play(row,col,color)
-    # print(sgfmill.ascii_boards.render_board(displayboard))
-
-    # print("Query result: ")
-    # for i in range(10000):
-    #     q = katago.query(board, moves, komi)
-
-    # katago.close()
-
 # %%
 
 def loc_to_query_loc(board, loc):
@@ -249,14 +183,13 @@ MODEL_PATH = "/home/ubuntu/katago_pessimize/kata1-b18c384nbt-s7709731328-d371529
 def get_query_move(color, loc, board):
     return ('_bw'[color], (board.loc_x(loc), board.loc_y(loc)) if loc != Board.PASS_LOC else "pass")
 
-def get_training_data_from_sgf(katago:KataGo, sgf_filename, min_move=0, max_move=300):
-    global rules
+def get_training_data_from_sgf(katago:KataGo, sgf_filename, min_move=50, max_move=150, verbose=False):
     metadata, setup, moves, rules = load_sgf_moves_exn(sgf_filename)
     print(f"Loaded {sgf_filename}")
     print(f"Metadata: {metadata}")
     print(f"Rules: {rules}")
     print(f"Setup: {setup}")
-    print(f"Moves: {moves}")
+    if verbose: print(f"Moves: {moves}")
 
     # katago = KataGo(args["katago_path"], args["config_path"], args["model_path"])
     query_board = sgfmill.boards.Board(19)
@@ -266,8 +199,7 @@ def get_training_data_from_sgf(katago:KataGo, sgf_filename, min_move=0, max_move
     # for move in setup:
     #     gs.play(move[0], move[1])
     # print(gs.board.to_string())
-    values = []
-    move_values = []
+    n_queries = 0
     start_time = time.time()
     # queries = 0
     for i, game_move in tqdm(enumerate(moves[:max_move])):
@@ -283,56 +215,50 @@ def get_training_data_from_sgf(katago:KataGo, sgf_filename, min_move=0, max_move
         for move in range(metadata.size ** 2):
             move = move + 19 + 1
             if gs.board.would_be_legal(gs.board.pla, move):
-                # print(query_moves)
-                # queries += 1
+                n_queries += 1
                 katago.query(query_board, query_moves + [get_query_move(gs.board.pla, move, gs.board)], komi)
-                # if queries > 5:
-                    # queries -= 1
-                # output = katago.get_response()
-                # if 'rootInfo' in output: values.append(output['rootInfo']["winrate"])
-                # if queries >= 100:
-                #     queries = 0
-
-        move_values.append(this_values)
 
     end_time = time.time()
     print(f"Querying finished in time: {end_time - start_time:.4f}. Now waiting for gpu...")
     # print(values)
-    return values, move_values, gs
-
-
-# get first file in SGF_DIR
-sgf_filename = os.listdir(SGF_DIR)[2]
-sgf_rel_path = os.path.join(SGF_DIR, sgf_filename)
-# sgf_filename = "blood_vomit.sgf"
-
-katago = KataGo(KATAGO_PATH, CONFIG_PATH, MODEL_PATH, sgf_filename)
-try:
-    cProfile.run('values, move_values, gs = get_training_data_from_sgf(katago, sgf_rel_path)', 'output.pstats')
-    # values, move_values, gs = get_training_data_from_sgf(katago, sgf_rel_path)
-    katago.close()
-except KeyboardInterrupt:
-    katago.close()
-    raise KeyboardInterrupt
-except Exception as e:
-    print(e)
-    katago.close()
-    raise e
-
-# print(gs.board.to_string())
+    return gs, n_queries
 
 # %%
 
-# sns.histplot(elapsed_out)
+def annotate_all_games(overwrite=False, max_games=None, verbose=False):
+    files = os.listdir(SGF_DIR)
+    if max_games is not None:
+        files = files[:max_games]
+    for sgf_filename in files:
+        print(f"Starting to annotate game {sgf_filename}")
+        sgf_rel_path = os.path.join(SGF_DIR, sgf_filename)
+        # sgf_filename = "blood_vomit.sgf"
+        annotation_rel_path = os.path.join(ANNOTATION_DIR, sgf_filename + '.stdout')
+        if os.path.exists(annotation_rel_path) and not overwrite:
+            print(f"Skipping {sgf_filename}")
+            continue
+        katago = KataGo(KATAGO_PATH, CONFIG_PATH, MODEL_PATH, annotation_rel_path, verbose=verbose)
+        try:
+            # cProfile.run('values, move_values, gs = get_training_data_from_sgf(katago, sgf_rel_path)', 'output.pstats')
+            gs, n_queries = get_training_data_from_sgf(katago, sgf_rel_path, verbose=verbose)
+            n_writes = katago.close()
+        except KeyboardInterrupt:
+            n_writes = katago.close()
+            raise KeyboardInterrupt
+        except Exception as e:
+            print(f"Exception: {e}")
+            n_writes = katago.close()
+        if n_queries != n_writes:
+            print(f"Warning: {n_queries} queries but {n_writes} writes on file {sgf_filename}")
+        
+annotate_all_games(overwrite=False, verbose=False, max_games=20)
+
 # %%
 
 # Create a pstats object
-p = pstats.Stats('output.pstats')
+# p = pstats.Stats('output.pstats')
 
 # Sort the statistics by the cumulative time and print the first few lines
-p.sort_stats('cumulative').print_stats(20)
+# p.sort_stats('cumulative').print_stats(20)
 
-# %%
-
-sns.lineplot(data=values[::2])
 # %%
